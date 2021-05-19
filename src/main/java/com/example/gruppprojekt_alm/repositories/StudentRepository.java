a2fe197ef4a046123f43d06d6b795f19b15b4910
”Daniel <dannekun@hotmail.com>
2021-05-19 20:05:18 +0200

    added CRUD method for controller

--- src/main/java/com/example/gruppprojekt_alm/repositories/StudentRepository.java
+++ src/main/java/com/example/gruppprojekt_alm/repositories/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.gruppprojekt_alm.repositories;
 
-public interface StudentRepository {
+import com.example.gruppprojekt_alm.models.Student;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Integer> {
 }
